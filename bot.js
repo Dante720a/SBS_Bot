add5e7d080b5768c90a7b96cb40493dd5a5803df
Dante720a <65083076+Dante720a@users.noreply.github.com>
2020-05-30 22:31:02 +0430

    Update bot.js

--- bot.js
+++ bot.js
@@ -220,7 +220,7 @@ bot.onText(/(.+)$/, function (msg, match) {
 			formattedAnswer += "\n";
                         			
                         itemsFound++;
-                    	formattedAnswer += "" + item.content.$t.substring(6, 100).replace(",", " |").replace("موجودی:", " ") + "" ; // add item content, '\u27a1' is the arrow emoji
+                    	formattedAnswer += "" + item.content.$t.substring(6, 100).replace(",", "  |  ").replace("موجودی:", " ") + "" ; // add item content, '\u27a1' is the arrow emoji
 			//FormattedAnswerF += item.content.$t; // add item content, '\u27a1' is the arrow emoji
 			
                 }
